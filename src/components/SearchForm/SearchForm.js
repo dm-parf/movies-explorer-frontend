fef7e21d49d72836f5445c7ff785c81576c5a8ef
Dmitry Parfentev <favoritegrech@yandex.ru>
2021-08-22 14:16:10 +0300

    level2 second try

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,18 +5,18 @@ import searchlogo from '../../images/search-icon.svg';
 function SearchForm(){
     return (
         <>
-        <div className="search__zone">
+        <form className="search__zone">
             <div className="search__all">
                 <img className="search__image" src={searchlogo} alt='search'/>
-                <input className="search__input"  placeholder="Фильм"></input>
-                <button className="search__button"></button> 
+                <input className="search__input" required="true" placeholder="Фильм"></input>
+                <button  type="submit" className="search__button"></button> 
             </div>
             <div className="search__shorts">
                 <hr className="search__verticla-line"></hr>
                 <FilterCheckbox></FilterCheckbox>
                 <p className="search__label">Короткометражки</p>
             </div>
-        </div>
+        </form>
         <hr className="search__line"></hr>
     </>
     );
